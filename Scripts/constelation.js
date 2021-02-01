83beddc219f1581c894f98d479fefdfc17be77ce
Kevin Breurken <kevinbreurken@gmail.com>
2021-02-01 14:29:09 +0100

    added satelites

--- Scripts/constelation.js
+++ Scripts/constelation.js
@@ -1,11 +1,16 @@
+//Space
 const backgroundColor = '#0D1418';
 Pts.namespace(window);
 var space = new CanvasSpace("#space");
 var form = space.getForm();
-createSpace();
+// attach handler to the click event of the document
+if (document.attachEvent) document.attachEvent('onclick', handler);
+else document.addEventListener('mousemove', handler);
+
 const screenWidth = screen.width;
 const screenHeight = screen.height;
 
+createSpace();
 //Create an space..
 function createSpace() {
     space.setup({
@@ -14,24 +19,21 @@ function createSpace() {
         autoResize: true
     });
 
-
-    space.bindMouse().bindTouch().play();
     createStars();
+    space.bindMouse().bindTouch().play();
 }
 
 function createStars() {
 
     let maxZDepth = 10;
-    const starsWidthSpacing = 100;
-    const starsHeightSpacing = 100;
+    const starsWidthSpacing = 80;
+    const starsHeightSpacing = 80;
 
-    const maxStarSize = 10;
-    const maxForegroundStars = 10;
+    const maxForegroundStars = 5;
 
     let backgroundStars = [];
     let foregroundStars = [];
 
-    let constelationLines = [];
 
     space.add({
         start: (bound) => {
@@ -40,7 +42,7 @@ function createStars() {
                 for (let y = 0; y < Math.ceil((screenHeight / starsHeightSpacing)); y++) {
                     const randomOffsetx = (starsWidthSpacing - 10) * Math.random();
                     const randomOffsety = (starsHeightSpacing - 10) * Math.random();
-                    const zDepth = Math.round(4 * Math.random());
+                    const zDepth = Math.round(4* Math.random());
 
                     //Create an point
                     const bgStar = new BackgroundStar(x * (starsWidthSpacing) + randomOffsetx, (y * starsHeightSpacing) + randomOffsety, zDepth, zDepth);
@@ -56,7 +58,7 @@ function createStars() {
             for (let x = 0; x < maxForegroundStars; x++) {
                 const randomOffsetx = screenWidth * Math.random();
                 const randomOffsety = screenHeight * Math.random();
-                const zDepth = Math.round(maxZDepth - (3 * Math.random()));
+                const zDepth = Math.round(2 + (2 * Math.random()));
 
                 //Create an point
                 const fgStar = new ForegroundStar(randomOffsetx, randomOffsety, zDepth, zDepth);
@@ -82,6 +84,10 @@ function createStars() {
     });
 }
 
+let posDifX;
+
+let posDifY;
+
 class BackgroundStar {
 
     constructor(x, y, z, w) {
@@ -90,22 +96,19 @@ class BackgroundStar {
         this.Pt = new Pt(x, y, z, w);
         space.add(() => form.point(this.Pt, this.Pt.w * 0.7, "circle").fill(starColor).stroke(starColor).alpha(0.9));
     }
-
-    get area() {
-        return this.calcArea()
-    }
-
+    
     animate(time, ftime) {
         this.Pt.x += (0.1 * (this.Pt.z / 10) * posDifX) * ftime;
         this.Pt.y += (0.1 * (this.Pt.z / 10) * posDifY) * ftime;
+
         if (this.Pt.x > 2000)
             this.Pt.x = -10;
-        if(this.Pt.x < -10){
+        if (this.Pt.x < -10) {
             this.Pt.x = 2000;
         }
         if (this.Pt.y > 1080)
             this.Pt.y = -10;
-        if(this.Pt.y < -10){
+        if (this.Pt.y < -10) {
             this.Pt.y = 1080;
         }
     }
@@ -120,29 +123,34 @@ class ForegroundStar {
 
     constructor(x, y, z, w) {
         const starColor = '#ffffff';
+        const starBorder = '#ffd200';
+        const starTransparency = 0.9;
 
         this.Pt = new Pt(x, y, z, w);
         this.movementDirectionX = 0;
         this.movementDirectionY = 0;
-        space.add(() => form.point(this.Pt, this.Pt.w, "circle").fill(starColor).stroke(starColor).alpha(0.9));
-    }
-
-    get area() {
-        return this.calcArea()
+        space.add(() => form.point(this.Pt, this.Pt.w, "circle").fill(starColor).stroke(starBorder).alpha(starTransparency));
     }
 
     animate(time, ftime) {
-        this.Pt.x += (this.movementDirectionX * (this.Pt.z / 10)) * ftime;
-        this.Pt.y += (this.movementDirectionY * (this.Pt.z / 10)) * ftime;
+        //Default movement vector
+        this.Pt.x += (0.1 * (this.Pt.z / 10) * posDifX) * ftime;
+        this.Pt.y += (0.1 * (this.Pt.z / 10) * posDifY) * ftime;
+        //Add
+        this.Pt.x += (0.1 *  (this.movementDirectionX * (this.Pt.z / 10))) * ftime;
+        this.Pt.y += (0.1 *  (this.movementDirectionY * (this.Pt.z / 10))) * ftime;
+        // this.Pt.x += (this.movementDirectionX * (this.Pt.z / 10))* posDifX * ftime;
+        // this.Pt.y += (this.movementDirectionY * (this.Pt.z / 10))* posDifY * ftime;
 
-        if (this.Pt.x > 5000 || this.Pt.x < -5000) {
-            this.Pt.x = 2000 * -this.movementDirectionX;
-            this.Pt.y = 2000 * -this.movementDirectionY;
+        if (this.Pt.x > 2000)
+            this.Pt.x = -10;
+        if (this.Pt.x < -10) {
+            this.Pt.x = 2000;
         }
-
-        if (this.Pt.y > 5000 || this.Pt.y < -5000) {
-            this.Pt.x = 2000 * -this.movementDirectionX;
-            this.Pt.y = 2000 * -this.movementDirectionY;
+        if (this.Pt.y > 1080)
+            this.Pt.y = -10;
+        if (this.Pt.y < -10) {
+            this.Pt.y = 1080;
         }
     }
 
@@ -157,14 +165,10 @@ class ForegroundStar {
     }
 }
 
-// attach handler to the click event of the document
-if (document.attachEvent) document.attachEvent('onclick', handler);
-else document.addEventListener('mousemove', handler);
-var posDif;
-var posDifX = 0;
-var posDifY = 0;
+posDifX = (-1 + (2 * Math.random()));
+posDifY = (-1 + (2 * Math.random()));
+
 function handler(e) {
-    e = e || window.event;
 
     var pageX = e.pageX;
     var pageY = e.pageY;
@@ -175,8 +179,22 @@ function handler(e) {
         pageY = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
     }
 
-    posDif = Math.hypot((screenWidth / 2) - pageX, (screenHeight / 2) - pageY);
-    posDifX = -((screenWidth / 2) - pageX) * 0.1;
-    posDifY = -((screenHeight / 2) - pageY) * 0.1;
-    console.log(posDif);
+    //posDif = Math.hypot((screenWidth / 2) - pageX, (screenHeight / 2) - pageY);
+    if (mouseDown > 0) {
+        posDifX = lerp(posDifX, -((screenWidth / 2) - pageX) * 0.1, 0.1);
+        posDifY = lerp(posDifY, -((screenHeight / 2) - pageY) * 0.1, 0.1);
+    }
+    // console.log(posDif);
+}
+
+var mouseDown = 0;
+document.body.onmousedown = function () {
+    mouseDown = 1;
+}
+document.body.onmouseup = function () {
+    mouseDown = 0;
+}
+
+function lerp(start, end, amt) {
+    return (1 - amt) * start + amt * end
 }
